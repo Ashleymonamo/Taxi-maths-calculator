caf382d8074783a241d69ce7a8e499ef95267955
Ashleymonamo <ashleymonamo@gmail.com>
2023-10-13 01:37:52 -0800

    Alert

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,15 @@
-console.log("RUNNING");
+
 function CalculateSum() {
     
 let Fare=document.getElementById("fare").value;
-console.log(Fare);
+//console.log(Fare);
 let numOfPeople=document.getElementById("numPeople").value;
-console.log(numOfPeople);
+//console.log(numOfPeople);
 let MoneySent=document.getElementById("moneySent").value;
-console.log(MoneySent);
+//console.log(MoneySent);
 //calculate the sum per seats
  let sum=numOfPeople*Fare;
- console.log(sum);
+ //console.log(sum);
  let change=MoneySent-sum;
  let moneyShort=sum-MoneySent;
 //console.log(change);
@@ -17,9 +17,11 @@ console.log(MoneySent);
 if(MoneySent<sum)
 {
     console.log("Money is short with R" +moneyShort)
+    alert("Money is short with R" +moneyShort);
 }
 else{
-    console.log("Change is R"+change)
+    console.log("Change is R"+change);
+    alert("Change is R"+change);
 }
 
 }
